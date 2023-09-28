607951efa0b1ad8e6a6bdee6fb49c353dedbc537
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-09-28 07:04:07 +0700

    protecting the routes with Navigate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
+
 import Products from './pages/Products';
-import Navbar from './components/Navbar'
 import Add from './pages/Add';
 import Update from './pages/Update';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Navbar from './components/Navbar'
 
 function App() {
+  const {user} = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Products/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
+          <Route path='/' element={user ? <Products/> : <Navigate to='/login'/>}/>
+          <Route path='/login' element={!user ? <Login/> : <Navigate to='/'/>}/>
+          <Route path='/register' element={!user ? <Register/> : <Navigate to='/'/>}/>
           <Route path='/products' element={<Products/>}/>
           <Route path='/add' element={<Add/>}/>
           <Route path='/update' element={<Update/>}/>
